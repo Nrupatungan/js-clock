648f366144756b3f41f769cff551adab571afd16
Dinesh Sake <dineshsake12345@gmail.com>
2023-12-16 20:08:23 +0530

    changed transform origin of hands

--- app/js/script.js
+++ app/js/script.js
@@ -9,17 +9,17 @@ function setDate() {
     const now = new Date();
 
     const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    const secondsDegrees = ((seconds / 60) * 360);
     sec_hand.style.transform = `rotate(${secondsDegrees}deg)`;
     sec.innerText = seconds;
 
     const mins = now.getMinutes();
-    const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+    const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6);
     min_hand.style.transform = `rotate(${minsDegrees}deg)`;
     mint.innerText = mins;
 
     const hour = now.getHours();
-    const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+    const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30);
     hour_hand.style.transform = `rotate(${hourDegrees}deg)`;
     hr.innerText = hour;
 }
